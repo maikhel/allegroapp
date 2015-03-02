432091c87b7c13bc54a6c19cdfda04e48612e3b8
Michal Łęcicki <mlecicki@gmail.com>
2015-03-02 13:55:18 +0100

    added new methods

--- app/models/lol.rb
+++ app/models/lol.rb
@@ -4,10 +4,11 @@ class Lol
 	WSDL_URL = 'https://webapi.allegro.pl.webapisandbox.pl/service.php?wsdl'
 	LOCAL_VERSION = '1422452391'
 
-	attr_reader :data
+	attr_reader :data, :item_info
+	
 
 	def initialize
-
+		@item_info = Hash.new
 
 		client = Savon.client(wsdl: WSDL_URL,
 		 log: true,
@@ -15,22 +16,81 @@ class Lol
 		 pretty_print_xml: true,
 		 ssl_verify_mode: :none)
 
-		xml_message = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\"
-					 xmlns:urn=\"urn:SandboxWebApi\">
-		   <soapenv:Header/>
-		   <soapenv:Body>
-		      <urn:DoQuerySysStatusRequest>
-		         <urn:sysvar>4</urn:sysvar>
-		         <urn:countryId>1</urn:countryId>
-		         <urn:webapiKey>#{ENV["WEBAPIKEY"]}</urn:webapiKey>
-		      </urn:DoQuerySysStatusRequest>
-		   </soapenv:Body>
-		</soapenv:Envelope>"
+		xml_message = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:urn=\"urn:SandboxWebApi\">
+   <soapenv:Header/>
+   <soapenv:Body>
+      <urn:DoGetItemsListRequest>
+         <urn:webapiKey>sfe724d4</urn:webapiKey>
+         <urn:countryId>1</urn:countryId>
+         <!--Optional:-->
+         <urn:filterOptions>
+            <!--Zero or more repetitions:-->
+            <urn:item>
+               <!--You may enter the following 3 items in any order-->
+               <urn:filterId>category</urn:filterId>
+               <!--Optional:-->
+               <urn:filterValueId>
+                  <!--Zero or more repetitions:-->
+                  <urn:item>1</urn:item>
+               </urn:filterValueId>
+               <!--Optional:-->
+              
+            </urn:item>
+         </urn:filterOptions>
+         <!--Optional:-->
+         <urn:sortOptions>
+    
+            <urn:sortType>endingTime</urn:sortType>
+        
+         </urn:sortOptions>
+         <!--Optional:-->
+         <urn:resultSize>50</urn:resultSize>
+         
+      </urn:DoGetItemsListRequest>
+   </soapenv:Body>
+</soapenv:Envelope>"
+		
 
-		response = client.call(:do_query_sys_status, xml: xml_message)
+		response = client.call(:do_get_items_list, xml: xml_message)
+		# {:category_id=>"26013", :category_name=>"Antyki i Sztuka", 
+		# :category_parent_id=>"0", :category_items_count=>"0"}
+		data = response.to_hash
 
-		data = response.to_array(:do_query_sys_status_response).first
-		local_version = data[:ver_key]
+		data = data[:do_get_items_list_response][:items_list][:item].first
+		
+
+
+		@item_info[:item_id] = data[:item_id]
+		@item_info[:item_title] = data[:item_title]
+		@item_info[:left_count] = data[:left_count]
+		@item_info[:ending_time] = data[:ending_time]
+		@item_info[:time_to_end] = data[:time_to_end]
+		@item_info[:category_id] = data[:category_id]
+		@item_info[:condition_info] = data[:condition_info]
+
+		# link: nazwa + item_id
+		# http://allegro.pl.webapisandbox.pl/
+		#uchwyt-do-urzadzenia-mobilnego-7-10-1comfort-i5039448075.html
+		# data.each do |item|
+
+		# 	@item_info.store("item_id", item[:item_id])
+			
+		# end
+
+		 ns=>"0", :country_id=>"1"}, :price_info=>{:item=>[{:price_type=>"buyNow", :price_value=>"39"}, {:price_type=>"withDelivery", :price_value=>"50"}]}, :photos_info=>{:item=>[{:photo_size=>"small", :photo_url=>"http://img02.webapisandbox.pl/photos/64x48/50/39/44/80/5039448075", :photo_is_main=>true}, {:photo_size=>"medium", :photo_url=>"http://img02.webapisandbox.pl/photos/128x96/50/39/44/80/5039448075", :photo_is_main=>true}, {:photo_size=>"large", :photo_url=>"http://img02.webapisandbox.pl/photos/400x300/50/39/44/80/5039448075", :photo_is_main=>true}]}}
+
+		# [:categories_list][:categories_tree][:item]
+		# @data = []
+		# data.each do |item|
+		# 	@data << item[:category_name]
+		# 	@data << item[:category_items_count]
+		# end
+		
+
+		@data = @item_info
+
+		# data = response.to_array(:do_query_sys_status_response).first
+		# local_version = data[:ver_key]
 
 
 	end
--- app/models/product.rb
+++ app/models/product.rb
@@ -7,11 +7,13 @@ class Product
 	
 	WSDL_URL = 'https://webapi.allegro.pl.webapisandbox.pl/service.php?wsdl'
 	local_version = '1422452391'
-	attr_reader :it_id, :name, :ItBuyNowPrice, :ItCity, :data
+	country_id = 1
+	session_handle = 0
+	attr_reader :item_info
 	
 	def initialize
 		
-		client = Savon.client(wsdl: WSDL_URL,
+		@client = Savon.client(wsdl: WSDL_URL,
 		 log: true,
 		 log_level: :debug,
 		 pretty_print_xml: true,
@@ -76,6 +78,12 @@ class Product
 		
   	end
 
+  	def login
+
+
+
+  	end
+  	
 
   	def take_new_local_version
 
@@ -114,17 +122,6 @@ end
 
 
 
-
-
-
-
-
-
-
-
-
-
-
 # 		response = client.call(:do_get_countries, xml: "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:urn=\"urn:SandboxWebApi\">
 #    <soapenv:Header/>
 #    <soapenv:Body>
@@ -136,13 +133,3 @@ end
 # </soapenv:Envelope>")
 		
 		#response = client.call(:do_show_item_info_ext, message: {"sessionHandle" => 'eba5e813c25a50b40039d746d7b0ccc3c32745010bcc68me00_1',"itemId" => 5039456794})
-
-	
-		
-      		# data = response.to_array(:get_info_by_zip_response, :get_info_by_zip_result, :new_data_set, :table).first
-		      # if data
-		      #   @state = data[:state]
-		      #   @city = data[:city]
-		      #   @area_code = data[:area_code]
-		      #   @time_zone = data[:time_zone]
-		      # end
